459b7c1bebea20d0fc7569d93c162bb1d3729fdd
felipemaya <pipe.040691@gmail.com>
2023-01-20 02:16:44 -0500

    selectores id,class,name

--- 01-selectores.js
+++ 01-selectores.js
@@ -1,9 +1,10 @@
 
 $(function () {
-    //selector de id
+    //selector de id #
     const rojo = $("#rojo"); // lo podemos guradar en una variable pero no es necesario 
     let cssRojo = rojo.css("background", "red").css("color","white"); // podemos crear una referencia de la variable principal y modificar sus valores 
     
     $("#amarillo").css("background", "yellow").css("color","red"); // aqui hacemos lo mismo pero sin necesidad de almacenar en una variable
     $("#verde").css("background", "green").css("color","white");
+    //selector por nombre $("tag["name='value']")
 })
