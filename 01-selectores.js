7cbf0750530ca0dc8853e2b4ef201ac69ef47841
felipemaya <pipe.040691@gmail.com>
2023-01-24 22:48:20 -0500

    mas ejemplos de manipulacion de estilos a las etiquetas seleccionadolas por clase o id, tambien la seleccion de etiquetas de un array de etiquetas con el metodo .eq()

--- 01-selectores.js
+++ 01-selectores.js
@@ -1,10 +1,37 @@
 
-$(function () {
+$(document).ready(function () {
     //selector de id #
     const rojo = $("#rojo"); // lo podemos guradar en una variable pero no es necesario 
     let cssRojo = rojo.css("background", "red").css("color","white"); // podemos crear una referencia de la variable principal y modificar sus valores 
     
-    $("#amarillo").css("background", "yellow").css("color","red"); // aqui hacemos lo mismo pero sin necesidad de almacenar en una variable
-    $("#verde").css("background", "green").css("color","white");
+    $("#amarillo").css("background", "yellow")
+                    .css("color","red"); // aqui hacemos lo mismo pero sin necesidad de almacenar en una variable
+    $("#verde").css("background", "green")
+                    .css("color","white");
+
+    // Selectores de clase .
+    let mi_clase = $(".zebra");
+
+    console.log(mi_clase[1]);// en caso de tenr varios tributos con la clase entonces que hace seleccion de cada elemento como en un array,
+    //pero si lo que quieres es para manipularlo entonces con el metodo de jquery mi_clase.eq(2) lo puedo sellecionar con todos sus propiedades
+    console.log(mi_clase.eq(2));// este .eq() permite seleccionar un elemento jquery en el que podemos modificar sus atributos
+
+    $(".sin_borde").click(function() {
+        console.log("click dado");
+    })
+
+    // selectores de etiqueta <>
+    let parrafos = $('p').css('cursor', 'pointer');
+    // en el siguiente bloque de codigo que usa el evento click se utiliza para hacer un toggle con las etiquetas p  para poner o quitar la clase zebra y grande 
+    parrafos.click(function() {
+        let elemento = $(this); // inicializamos una variable this para evitar cargar el DOM 
+        if(!elemento.hasClass('zebra')){
+            elemento.addClass('zebra')
+                    .addClass('grande');
+        }else{
+            elemento.removeClass('zebra')
+                    .removeClass('grande');
+        }
+    });
     //selector por nombre $("tag["name='value']")
 })
