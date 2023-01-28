af35f250390a951c8417faad9671d681685253ab
felipemaya <pipe.040691@gmail.com>
2023-01-27 21:52:05 -0500

    mas ejemplos de los evemtos hover y click para usarm con jquery

--- 02-eventos.js
+++ 02-eventos.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
     
-    // MouserOver y MouseOut
+    // MouserOver y MouseOut con este ejercicio podemos observar como podemos utilizar los eventos de mouseOver y mouseOut con jquery
     let caja = $('#caja');
-
+    /*
     caja.mouseover(function(){
         $(this).css("background","red");
     });
 
     caja.mouseout(function(){
         $(this).css("background","green")
+    })
+    */
+    // Hover forma mas sencilla de utilizar los eventos anteriores en uno 
+
+    function fondoRojo() {
+        $(this).css("background","red");
+    }
+
+    function fondoVerde() {
+        $(this).css("background","green");
+    }
+
+    //hover
+    caja.hover(fondoRojo,fondoVerde);
+
+    //evento click y dobleclick
+    caja.click(function () {
+        $(this).css("background","blue").css("color", "white");
     })
 })
