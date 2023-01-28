c7f5b1dd628ae7b8ec8cdfde2601811ead391279
felipemaya <pipe.040691@gmail.com>
2023-01-28 12:52:55 -0500

    mas ejemplos con los diferente metodos que tiene la libreria jquery mousedown,mouseup,mousemove

--- 02-eventos.js
+++ 02-eventos.js
@@ -1,3 +1,5 @@
+//const { event } = require("jquery");
+
 $(document).ready(function(){
     
     // MouserOver y MouseOut con este ejercicio podemos observar como podemos utilizar los eventos de mouseOver y mouseOut con jquery
@@ -28,4 +30,47 @@ $(document).ready(function(){
     caja.click(function () {
         $(this).css("background","blue").css("color", "white");
     })
+
+    caja.dblclick(function () {
+        $(this).css("background","pink").css("color", "yellow");
+    })
+
+    //evento focus blur
+
+    let inputNombre = $('#nombre');
+    let datos = $('#datos')
+
+    inputNombre.focus(function () { // focus cuando estas en el elemento
+        $(this).css("border", "5px solid green");
+    });
+
+    inputNombre.blur(function () { // cuando sales del elemento y escojes otro
+        $(this).css("border", "5px solid red");
+        let value = $(this).val(); // captura el valor de la eqtiqueta
+        datos.text(value) // con .text() podemos insertar datos enlos elementos
+        .show() // muestra un elemento que esta invisile "none"
+        
+
+    });
+
+    //mousedown mouseup
+
+    datos.mousedown(function () { // esqcuha el evento de presionar el click izquierdo y realiza una accion
+        $(this).css("background","greenyellow");
+    });
+
+    datos.mouseup(function () { // esqcuha el evento de soltar el click iz y realiza una accion
+        $(this).css("background","#ccc");
+
+    });
+    
+    // mousemove se puede capturar las coordenadas del mouse
+    let sigueme = $("#sigueme"); 
+    $(document).mousemove(function () {
+        console.log("x: ",event.clientX," y: ", event.clientY);
+        $('body').css("cursor", "none"); // podemos cultar el cursor y que la circunferencia funcione como cursor
+        sigueme.css("left", event.clientX) // le asignamos la ubicacion al elemento sigueme
+               .css("top", event.clientY);
+    })
+
 })
