3c5f12bd309caecb5ca7d0a7457594881464117b
felipemaya <pipe.040691@gmail.com>
2023-02-03 05:14:42 -0500

    se realiza llamado de libreria de la libreria de jquery UI para realizar varios ejemplos de uso con losmetodos draggable y resizable par poder iterractuar con los elementos

---
+++ 06-interface.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // con este metodo de jquery ui podemos arrastrar los elementos a traves de la pantalla 
+    $(".elemento").draggable();
+
+    // redimensionar con este metodo podemos redimensionar los elementos
+    
+    $(".elemento").resizable();
+})
